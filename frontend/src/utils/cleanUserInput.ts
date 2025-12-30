a0079d7eaccbfbb545103f676cf31fe97ab681cb
Damien <damienroot@icloud.com>
2025-12-30 05:27:33 +0000

    create util cleanUserInput

---
+++ frontend/src/utils/cleanUserInput.ts
@@ -0,0 +1,31 @@
+// expected input format: login - minutes - seconds - 24 hour time - task - station - manager
+// example: rooty 25m 14s 19:55 Each Transfer In dz-P-A3122 donty
+
+import { type FormattedMiss } from "../types/FormattedMiss";
+
+const cleanUserInput = (unformattedInput: string) => {
+  const formattedInput = unformattedInput
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, " ")
+    // consistency: removes any whitespace amount, adds a single space
+    .replaceAll("each transfer in", "each-transfer-in")
+    .replaceAll("stow to prime", "stow-to-prime")
+    .split(" ");
+
+  const formattedMisses: FormattedMiss[] = [];
+
+  const segmentsSize = 7;
+  for (let i = 0; i < formattedInput.length; i += segmentsSize) {
+    const segment = formattedInput.slice(i, i + segmentsSize);
+    formattedMisses.push({
+      login: segment[0],
+      missTime: segment[3],
+      station: segment[5].replace("dz-p-a", ""),
+    });
+  }
+
+  return formattedMisses;
+};
+
+export default cleanUserInput;
