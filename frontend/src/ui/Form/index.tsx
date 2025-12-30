04a3a23893a07851939c033277328a0155417eab
Damien <damienroot@icloud.com>
2025-12-30 05:42:11 +0000

    create form

---
+++ frontend/src/ui/Form/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Button, TextInput } from "@mantine/core";
+import formatUserInput from "../../utils/formatUserInput";
+import MissesTables from "../MissesTables";
+import { type FormattedMiss } from "../../types/FormattedMiss";
+
+const Form = () => {
+  const [formValue, setFormValue] = useState<string>("");
+  const [formattedMisses, setFormattedMisses] = useState<FormattedMiss[]>([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormattedMisses(formatUserInput(formValue));
+    setFormValue("");
+  };
+
+  const handleClick = () => {
+    setFormattedMisses(formatUserInput(formValue));
+    setFormValue("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          placeholder="Paste data here..."
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <Button onClick={handleClick}>Format</Button>
+      </form>
+      <MissesTables formattedMisses={formattedMisses} />
+    </div>
+  );
+};
+
+export default Form;
