e29040bc53f3e72cb9c1a26bb9e3aefd679eea22
Damien <damienroot@icloud.com>
2025-12-31 03:55:13 +0000

    add margin container to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import { Container } from "@mantine/core";
 import Form from "./ui/Form";
 
 const App = () => {
   return (
     <div>
-      <h1>Fast start formatter</h1>
-      <Form />
+      <Container size="xl">
+        <h1>Fast start formatter (stow only)</h1>
+        <Form />
+      </Container>
     </div>
   );
 };
